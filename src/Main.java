15685251b5ff359b0ed814fb882bbe34d236dd3f
Kov4lnk <kov4lnk@gmail.com>
2021-01-24 12:06:30 +0200

    added test conditions into main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(DigitSum.sumDigits(125));
+        System.out.println(DigitSum.sumDigits(-1));
+        System.out.println(DigitSum.sumDigits(4));
+        System.out.println(DigitSum.sumDigits(32123));
     }
 }
